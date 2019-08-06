d64be72662a4f41cce48c4c3f86f7ac919f2fadc
MrRen <543169072@qq.com>
2019-08-06 09:57:06 +0800

    init

--- src/App.php
+++ src/App.php
@@ -4,9 +4,9 @@ namespace epii\server;
 
 
 use epii\server\i\IArgsKeys;
-use epii\server\i\IParamCheck;
+
 use epii\server\i\IRun;
-use think\Db;
+
 
 
 /**
@@ -207,7 +207,7 @@ class App
                 Args::setKeysForArgValues($run->keysForArgValues());
             }
 
-            $this->param_check($run);
+
 
             if (method_exists($run, $m)) {
                 $this->runner_method = $m;
@@ -241,23 +241,6 @@ class App
     }
 
 
-    private function param_check($run)
-    {
-        if ($run instanceof IParamCheck) {
-            $must_parame = $run->param_get_required();
-            $e_param = [];
-            if ($must_parame) {
-                foreach ($must_parame as $value) {
-                    if (Args::params($value) === null) {
-                        $e_param[] = $value;
-                    }
-                }
-            }
-            if ($ok = (count($e_param) > 0)) {
-                $run->param_on_result($ok, $e_param);
-            }
-        }
-    }
 
     private function beforRun()
     {
